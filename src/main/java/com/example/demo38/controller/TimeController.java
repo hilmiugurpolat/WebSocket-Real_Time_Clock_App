f8f4c40e847e84299253f10b4107294a4ed5c207
hilmi uğur polat <hilmip13@gmail.com>
2023-08-15 16:51:05 +0300

    .

--- src/main/java/com/example/demo38/controller/TimeController.java
+++ src/main/java/com/example/demo38/controller/TimeController.java
@@ -1,18 +1,37 @@
 package com.example.demo38.controller;
-//Real-Time Clock App
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Controller
+@EnableScheduling
 public class TimeController {
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private String currentTime = dateFormat.format(new Date());
+
+    private final SimpMessagingTemplate template;
+
+    public TimeController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
 
     @MessageMapping("/time")
     @SendTo("/topic/time")
     public String getTime() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-        return dateFormat.format(new Date());
+        return currentTime;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void updateTime() {
+        currentTime = dateFormat.format(new Date());
+        template.convertAndSend("/topic/time", currentTime);
     }
 }
